c27ed4731d3ea0c7a9bdf6290c2a514f47e29ae1
swayam <goura0775@gmail.com>
2024-07-18 12:07:09 +0530

    ad

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+import withPWA from 'next-pwa';
+
+const pwaConfig = {
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
 };
 
-export default nextConfig;
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['fdn2.gsmarena.com', 'fdn.gsmarena.com', 'data.phoneo.in'],
+  },
+};
+
+export default withPWA(pwaConfig)(nextConfig);
+
+
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js');
+      });
+    }
+  }, []);
   return <Component {...pageProps} />;
 }
